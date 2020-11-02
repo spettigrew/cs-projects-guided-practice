125be5fb53cafc120c7eb5e349f38eb7982f88d0
Jess <jessbonanno@gmail.com>
2020-11-02 11:08:35 -0500

    finished galvanize

--- practice.js
+++ practice.js
@@ -555,4 +555,129 @@ function calculateAveragePricePerDesigner(inventory) {
 // Create helper functions if needed
 
 
-console.log(calculateAveragePricePerDesigner(currentInventory))
+// console.log(calculateAveragePricePerDesigner(currentInventory))
+
+
+/*
+
+You have a fashion catalog, an inventory of items from various high-fashion designers. Each designer has a lineup of shoes. Each shoe has a name and a price.
+
+It looks like this:
+
+var currentInventory = [
+  {
+    name: 'Brunello Cucinelli',
+    shoes: [
+      {name: 'tasselled black low-top lace-up', price: 1000},
+      {name: 'tasselled green low-top lace-up', price: 1100},
+      {name: 'plain beige suede moccasin', price: 950},
+      {name: 'plain olive suede moccasin', price: 1050}
+    ]
+  },
+  {
+    name: 'Gucci',
+    shoes: [
+      {name: 'red leather laced sneakers', price: 800},
+      {name: 'black leather laced sneakers', price: 900}
+    ]
+  }
+];
+1
+Fashion Inventory Part-C
+Your task is to find all of the shoes with "black" in the name. Your function should filter these shoes, and return them in a "flat list" similarly to Part A. Here is an example of the console output:
+
+Brunello Cucinelli, tasselled black low-top lace-up, 1000
+Gucci, black leather laced sneakers, 900
+Here is an example of a flat list in code:
+
+var flatList = "First line\nSecond Line\nThird Line\n";
+console.log(flatList);
+Observe that a "flat list" refers to a string where each new line is separated by the newline symbol. Also note that the "flat list" ends with a newline symbol. There are, like all of the challenges in this course, tests attached to these exercises. However, in order to get the most effective practice, please continue to write your own unit tests.*/
+
+function listAllBlackShoes(inventory) {
+  let flatBlackList = ''
+  inventory.map(designer => {
+    designer.shoes.map(shoe => {
+      if (shoe.name.includes('black')) {
+
+        flatBlackList += `${designer.name}, ${shoe.name}, ${shoe.price}\n`
+      }
+    })
+  })
+  return flatBlackList
+}
+
+// console.log(listAllBlackShoes(currentInventory))
+
+
+/*
+*  The task now is to find all the laced shoes, but we are going to render them in a somewhat complex format. Your function should return shoe names that contain "lace" in them, and indicate which word contains "lace". The return value's format should be structured like this:
+
+var expectedResult = [
+  {
+    "nameWords": [
+      "tasselled",
+      "black",
+      "low-top",
+      "lace-up"
+    ],
+    "targetWordIndex": 3
+  },
+  {
+    "nameWords": [
+      "tasselled",
+      "green",
+      "low-top",
+      "lace-up"
+    ],
+    "targetWordIndex": 3
+  },
+  {
+    "nameWords": [
+      "red",
+      "leather",
+      "laced",
+      "sneakers"
+    ],
+    "targetWordIndex": 2
+  },
+  {
+    "nameWords": [
+      "black",
+      "leather",
+      "laced",
+      "sneakers"
+    ],
+    "targetWordIndex": 2
+  }
+];
+There are no tests attached to this exercise. In order to get the most effective practice, please continue to write your own unit tests.*/
+
+// create everything for this problem, functions, assertion function, and test cases
+// call your function "generateLaceDetails", and let it take the inventory as its parameter
+
+
+function listLacedShoes(inventory) {
+  let lacedShoes = []
+  inventory.map(designer => {
+    designer.shoes.map(shoe => {
+      if (shoe.name.includes('lace')) {
+        let shoes = shoe.name.trim().split(' ')
+        let laceIndex
+        shoes.map(str => {
+          if (str.includes('lace')) {
+            console.log(str)
+            laceIndex = shoes.indexOf(str)
+          }
+        })
+        lacedShoes.push({
+          'nameWords': shoes,
+          'targetWordIndex': laceIndex
+        })
+      }
+    })
+  })
+  return lacedShoes
+}
+
+console.log(listLacedShoes(currentInventory))
